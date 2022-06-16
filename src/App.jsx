16955695d8047696a2f7f924f54b9ec159ba35a8
Maria Meyer <mbean1216@icloud.com>
2022-06-16 12:04:49 -0500

    pseudocoding

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Portfolio Container
+    </div>
+  );
+}
+
+export default App;
+
+// TODO
+// Create Header (name and nav: about me, portfolio, contact, resume)
+// title corresponding to  the current section is highlighted
+// Onclick, the nav tabs shouldn't reload page
+// A single Header component that appears on multiple pages
+// A single Navigation component within the header that will be used to conditionally render the different sections of your portfolio
+
+// When porfolio loads, about me title and section selected by default
+// Must include recent photo and short bio in about me
+// In porfolio section, 6 images and links to applications and github repo
+// A single Project component that will be used multiple times in the Portfolio section
+
+// Contact section with form including name, email and message
+// If field is left blank, need notification that the field is required
+// Validation on Email and notification if not correct
+
+// Resume section with link to downloadable resume and list of proficiencies
+
+// A single Footer component that appears on multiple pages
+// Footer to  include icon links to GitHub  and LinkedIn profiles
