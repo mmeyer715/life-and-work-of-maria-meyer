f9abe932a83365019ad55b4d5092626cbaeb97fc
Maria Meyer <mbean1216@icloud.com>
2022-06-20 21:34:38 -0500

    portfolio not updating

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -2,64 +2,32 @@ import React from "react";
 import sleepily from "../../assets/about/sleepily.png"
 
 function Portfolio() {
-    const data = [
-        {
-            src: '',
-            title: '#about-me',
-            technologies: 'About',
-            gitRepo: '',
-            deployedApp: '',
-        },
-        {
-            src: { sleepily },
-            title: 'Sleepily',
-            technologies: 'Express.js, Handlebars, Express-Session, mySQL, sequelize',
-            gitRepo: 'https://github.com/mmeyer715/active-slumber',
-            deployedApp: 'https://sleepy-cove-47573.herokuapp.com/login',
-        },
-        {
-            src: '',
-            title: '#about-me',
-            technologies: 'About',
-            gitRepo: '',
-            deployedApp: '',
-        },
-        {
-            src: '',
-            title: '#about-me',
-            technologies: 'About',
-            gitRepo: '',
-            deployedApp: '',
-        },
-        {
-            src: '',
-            title: '#about-me',
-            technologies: 'About',
-            gitRepo: '',
-            deployedApp: '',
-        },
-        {
-            src: '',
-            title: '#about-me',
-            technologies: 'About',
-            gitRepo: '',
-            deployedApp: '',
-        },
-    ]
-    data.map((fields, index) => {
-        return (
-            <div key={index} className="card bg-dark text-white">
-                <img className="card-img" src={fields.src} alt="" />
-                <div className="card-img-overlay">
-                    <h5 className="card-title">{fields.title}</h5>
-                    <p className="card-text">Technologies Used: {fields.technologies}</p>
-                    <p className="card-text">GitHub: {fields.gitRepo}</p>
-                    <p className="card-text">Deployed: {fields.deployedApp}</p>
-                </div>
-            </div>
-        )
-    });
-
-};
+    // const data = [
+    //     {
+    //         img: { sleepily },
+    //         title: 'Sleepily',
+    //         technologies: 'Express.js, Handlebars.js, Express-Session, mySQL, sequelize, JawsDB, Heroku',
+    //         gitHub: 'https://github.com/mmeyer715/active-slumber',
+    //         deployedApp: 'https://sleepy-cove-47573.herokuapp.com/'
+    //     }
+    // ]
+    return (
+        // <div>
+        //     {data.map((fields, index) => {
+        //         return (
+                    <div className="card bg-dark text-white">
+                        <img className="card-img" src={sleepily} alt="" />
+                            <div className="card-img-overlay">
+                                <h5 className="card-title">Sleepily</h5>
+                                <p className="card-text">Technologies Used: Express.js, Handlebars.js, Express-Session, mySQL, sequelize, JawsDB, Heroku</p>
+                                <p className="card-text">GitHub: https://github.com/mmeyer715/active-slumber </p>
+                                <p className="card-text">Deployed URL: https://sleepy-cove-47573.herokuapp.com/ </p>
+                            </div>
+                    </div>
+        //         )
+        //     })}
+        // </div>
+    )
+}
 
 export default Portfolio;
