d7e4541d2c82a56d3f644a8d8a58f1579f7092b3
Maria Meyer <mbean1216@icloud.com>
2022-06-26 21:45:12 -0500

    deploying to git

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,69 +3,80 @@ import { validateEmail } from "../../utils/helpers";
 
 
 function Contact() {
-    // const [errMsg, setErrMsg] = useState('');
-    // const [formInfo, setFormInfo] = useState({ name: '', email: '', message: '' });
-    // const { name, email, message } = formInfo;
+    const [errMsg, setErrMsg] = useState('');
+    const [formInfo, setFormInfo] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formInfo;
 
-    // function handleBlur(event) {
-    
+    function handleBlur(event) {
+        if (event.target.name === 'email') {
+            const valid = validateEmail(event.target.value);
+
+            if (!valid) {
+                setErrMsg('Your email is invalid!');
+            } else {
+                setErrMsg('');
+            }
+        } else {
+            if (!event.target.value.length) {
+                setErrMsg(`${event.target.name} is required`);
+            } else {
+                setErrMsg('');
+            }
+        }
+        setFormInfo({ ...formInfo, [event.target.name]: event.target.value })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <div>
-            test
-        </div>
-    )
+        <section>
+            <form id="contact-htmlForm" onSubmit={handleSubmit}>
+                <div className="htmlForm-group">
+                    <div className="row">
+                            <label htmlFor='name'>Name:</label>
+                            <input type="text" defaultValue={name} onBlur={handleBlur} className="htmlForm-control" placeholder="Example: John Doe" name='name' />
+                    </div>
+                    <div className="row">
+                        <label htmlFor='email'>Email Address:</label>
+                        <input type="email" defaultValue={email} onBlur={handleBlur} className='htmlForm-control' placeholder="email@email.com" name="email" />
+                    </div>
+                    <div className="row">
+                        <label htmlFor='message'>Write message here:</label>
+                        <textarea defaultValue={message} onBlur={handleBlur} className="htmlForm-control" name="message"></textarea>
+                    </div>
+                </div>
+                {errMsg && (
+                    <div>
+                        <p>{errMsg}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+            
+            <div className="flex-contact">
+                <h1>Additional Contact InhtmlFormation</h1>
+                <div className="contact-info">
+                    <ul>
+                        <p>
+                            662-545-7566
+                        </p>
+                        <p>
+                            <a href="https://www.linkedin.com/in/maria-meyer-136484227/" rel="linkedin account">
+                                Linkedin
+                            </a>
+                        </p>
+                        <p>
+                            <a href="mbean1216@icloud.com" rel="personal email">
+                                mbean1216@icloud.com
+                            </a>
+                        </p>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
 }
-//         <section>
-//             <htmlForm id="contact-htmlForm" onSubmit={handleSubmit}>
-//                 <div className="htmlForm-group">
-//                     <div className="row">
-//                         <div className="col">
-//                             <label htmlFor='firstName'>First Name:</label>
-//                             <input type="text" defaultValue={firstName} onBlur={handleBlur} className="htmlForm-control" placeholder="First name" name='firstName' />
-//                         </div>
-//                         <div className="col">
-//                             <label htmlFor="lastName">Last Name:</label>
-//                             <input type="text" defaultValue={las} onBlur={handleBlur} className="htmlForm-control" placeholder="Last name" name="lastName" />
-//                         </div>
-//                     </div>
-//                     <div className="row">
-//                         <label htmlhtmlFor='emailInput'>Email Address:</label>
-//                         <input type="email" defaultValue={email} onBlur={handleBlur} className='htmlForm-control' placeholder="email@email.com" name="email" />
-//                     </div>
-//                     <div className="row">
-//                         <label htmlFor='messageInput'>Write message here:</label>
-//                         <textarea defaultValue={message} onBlur={handleBlur} className="htmlForm-control" name="message"></textarea>
-//                     </div>
-//                 </div>
-//                 {errMsg && (
-//                     <div>
-//                         <p>{errMsg}</p>
-//                     </div>
-//                 )}
-//                 <button type="submit">Submit</button>
-//             </htmlForm>
-//             <div className="flex-contact">
-//                 <h1>Additional Contact InhtmlFormation</h1>
-//                 <div className="contact-info">
-//                     <ul>
-//                         <p>
-//                             662-545-7566
-//                         </p>
-//                         <p>
-//                             <a href="https://www.linkedin.com/in/maria-meyer-136484227/" rel="linkedin account">
-//                                 Linkedin
-//                             </a>
-//                         </p>
-//                         <p>
-//                             <a href="mbean1216@icloud.com" rel="personal email">
-//                                 mbean1216@icloud.com
-//                             </a>
-//                         </p>
-//                     </ul>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// }
 
 export default Contact;
