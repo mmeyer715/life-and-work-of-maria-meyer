d1127184dc8bb86f00593f7b202bb141df52aad7
Maria Meyer <mbean1216@icloud.com>
2022-06-26 21:22:36 -0500

    testing deployment

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -3,20 +3,20 @@ import React from "react";
 function HeaderNav({ currentPage, handlePageChange }) {
     const data = [
         {
-            href: '#about',
-            pageName: 'About',
+            href: "#about",
+            pageName: "About",
         },
         {
-            href: '#portfolio',
-            pageName: 'Portfolio',
+            href: "#portfolio",
+            pageName: "Portfolio",
         },
         {
-            href: '#contact',
-            pageName: 'Contact',
+            href: "#contact",
+            pageName: "Contact",
         },
         {
-            href: '#resume',
-            pageName: 'Resume',
+            href: "#resume",
+            pageName: "Resume",
         },
     ]
     return (
@@ -28,14 +28,14 @@ function HeaderNav({ currentPage, handlePageChange }) {
                     </h1>
                 </div>
                 <div className="navLinks col">
-                    <ul className='navBar nav nav-tabs'>
+                    <ul className="navBar nav nav-tabs">
                         {data.map((fields, index) => {
                             return (
-                                <li key={index} className='nav-item'>
+                                <li key={index} className="nav-item">
                                     <a
                                         href={fields.href}
                                         onClick={() => handlePageChange(fields.pageName)}
-                                        className={currentPage === fields.pageName ? 'nav-link active' : 'nav-link'}
+                                        className={currentPage === fields.pageName ? "nav-link active" : "nav-link"}
                                     >
                                         {fields.pageName}
                                     </a>
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,61 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 
 function Contact() {
-    // const [formInfo, setFormInfo] = useState({
-    //     firstName: ''
-    // })
-    // const handleBlur = (event) => {
-    //     console.log(event.target.value.length);
-    // }
+    // const [errMsg, setErrMsg] = useState('');
+    // const [formInfo, setFormInfo] = useState({ name: '', email: '', message: '' });
+    // const { name, email, message } = formInfo;
 
+    // function handleBlur(event) {
+    
     return (
-
-        <section>
-            <form>
-                <div className="form-group">
-                    <div className="row">
-                        <div className="col">
-                            <label for='firstName'>First Name:</label>
-                            <input type={'tesxt'}  className="form-control" placeholder="First name" name='firstName' />
-                        </div>
-                        <div className="col">
-                            <label for="lastName">Last Name:</label>
-                            <input type={'text'} className="form-control" placeholder="Last name" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <label for='emailInput'>Email Address:</label>
-                        <input type={'email'} className='form-control' placeholder="email@email.com" />
-                    </div>
-                    <div className="row">
-                        <label for='messageInput'>Write message here:</label>
-                        <textarea className="form-control"></textarea>
-                    </div>
-                </div>
-            </form>
-            <div className="flex-contact">
-                <h1>Additional Contact Information</h1>
-                <div className="contact-info">
-                    <ul>
-                        <p>
-                            662-545-7566
-                        </p>
-                        <p>
-                            <a href="https://www.linkedin.com/in/maria-meyer-136484227/" rel="linkedin account">
-                                Linkedin
-                            </a>
-                        </p>
-                        <p>
-                            <a href="mbean1216@icloud.com" rel="personal email">
-                                mbean1216@icloud.com
-                            </a>
-                        </p>
-                    </ul>
-                </div>
-            </div>
-        </section>
-    );
+        <div>
+            test
+        </div>
+    )
 }
+//         <section>
+//             <htmlForm id="contact-htmlForm" onSubmit={handleSubmit}>
+//                 <div className="htmlForm-group">
+//                     <div className="row">
+//                         <div className="col">
+//                             <label htmlFor='firstName'>First Name:</label>
+//                             <input type="text" defaultValue={firstName} onBlur={handleBlur} className="htmlForm-control" placeholder="First name" name='firstName' />
+//                         </div>
+//                         <div className="col">
+//                             <label htmlFor="lastName">Last Name:</label>
+//                             <input type="text" defaultValue={las} onBlur={handleBlur} className="htmlForm-control" placeholder="Last name" name="lastName" />
+//                         </div>
+//                     </div>
+//                     <div className="row">
+//                         <label htmlhtmlFor='emailInput'>Email Address:</label>
+//                         <input type="email" defaultValue={email} onBlur={handleBlur} className='htmlForm-control' placeholder="email@email.com" name="email" />
+//                     </div>
+//                     <div className="row">
+//                         <label htmlFor='messageInput'>Write message here:</label>
+//                         <textarea defaultValue={message} onBlur={handleBlur} className="htmlForm-control" name="message"></textarea>
+//                     </div>
+//                 </div>
+//                 {errMsg && (
+//                     <div>
+//                         <p>{errMsg}</p>
+//                     </div>
+//                 )}
+//                 <button type="submit">Submit</button>
+//             </htmlForm>
+//             <div className="flex-contact">
+//                 <h1>Additional Contact InhtmlFormation</h1>
+//                 <div className="contact-info">
+//                     <ul>
+//                         <p>
+//                             662-545-7566
+//                         </p>
+//                         <p>
+//                             <a href="https://www.linkedin.com/in/maria-meyer-136484227/" rel="linkedin account">
+//                                 Linkedin
+//                             </a>
+//                         </p>
+//                         <p>
+//                             <a href="mbean1216@icloud.com" rel="personal email">
+//                                 mbean1216@icloud.com
+//                             </a>
+//                         </p>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </section>
+//     );
+// }
 
 export default Contact;
